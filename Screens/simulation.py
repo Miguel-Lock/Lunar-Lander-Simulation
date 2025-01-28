6c55795387ad32abdde0034759f2e0ce761ba660
Miguel-Lock <miguellock333@gmail.com>
2025-01-28 11:07:33 -0500

    Added comments to files

--- Screens/simulation.py
+++ Screens/simulation.py
@@ -1,16 +1,19 @@
 import pygame
 from game_state_manager import BaseState
 
+# Simulation screen
 class Simulation(BaseState):
     def run(self):
         self.display.fill('orange')
 
+        # Text with directions
         font = pygame.font.Font(None, 36)
         text_surface = font.render("Simulation. 1 for menu, 5 to quit", True, (255, 255, 255))  # White text
         text_rect = text_surface.get_rect()
         text_rect.center = (self.display.get_width() // 2, self.display.get_height() // 2)  # Center on screen
         self.display.blit(text_surface, text_rect)
 
+        # Key press detection
         keys = pygame.key.get_pressed()
         if keys[pygame.K_1]:
             self.gameStateManger.set_state('menu')
