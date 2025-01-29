2a3e7b540ed32b73d953370ad924a20979c0a7cb
Joshua <joshfouch777@gmail.com>
2025-01-29 12:51:52 -0500

    added screen menu background into menu.py

--- Screens/menu.py
+++ Screens/menu.py
@@ -1,16 +1,24 @@
 import pygame
 from game_state_manager import BaseState
 
-#main menu screen
+# main menu screen
+
+
 class Menu(BaseState):
     def run(self):
-        self.display.fill('red')
+        self.background = pygame.image.load(
+            "Screens/backgrounds/start_background.png")
+        self.display.blit(self.background, (0, 0))
+        # self.display.fill('red')
 
         # Text with directions
         font = pygame.font.Font(None, 36)
-        text_surface = font.render("Menu. 2 for customize, 3 for history, 5 to quit", True, (255, 255, 255))  # White text
+        text_surface = font.render(
+            # White text
+            "Menu. 2 for customize, 3 for history, 5 to quit", True, (255, 255, 255))
         text_rect = text_surface.get_rect()
-        text_rect.center = (self.display.get_width() // 2, self.display.get_height() // 2)  # Center on screen
+        text_rect.center = (self.display.get_width() // 2,
+                            self.display.get_height() // 2)  # Center on screen
         self.display.blit(text_surface, text_rect)
 
         # Key press detection
