67f9fdc7a4a7b2b37f3c438293ca0e0979fae7d6
Joshua <joshfouch777@gmail.com>
2025-03-31 20:28:35 -0400

    idk, what I did here

--- gui_code/buttons.py
+++ gui_code/buttons.py
@@ -1,5 +1,5 @@
 import pygame
-from constants import SCREEN
+from constants import SCREEN, FONT
 
 # instantiate the images as variables
 start_button_img = pygame.image.load(
@@ -12,8 +12,6 @@ backtomenu_button_img = pygame.image.load(
     'gui_code/buttons/BacktoMenu.png').convert_alpha()
 
 # button class to declare button instances
-
-
 class Button():
     def __init__(self, x, y, image, scale):
 
@@ -51,3 +49,42 @@ class Button():
         SCREEN.blit(self.image, (self.rect.x, self.rect.y))
 
         return action
+
+
+class InputBox:
+    def __init__(self, x, y, width, height, input=''):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.color_inactive = pygame.Color('lightskyblue3')
+        self.color_active = pygame.Color('dodgerblue2')
+        self.color = self.color_inactive
+        self.font = FONT
+        self.text = input
+        self.txt_surface = self.font.render(self.text, True, self.color)
+        self.active = False
+        self.input = input
+
+    def handle_event(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if self.rect.collidepoint(event.pos):
+                self.active = True
+                self.color = self.color_active
+            else:
+                self.active = False
+                self.color = self.color_inactive
+        
+        if event.type == pygame.KEYDOWN and self.active:  # Only handle key events if active
+            if event.key == pygame.K_BACKSPACE:
+                self.text = self.text[:-1]
+            elif event.key == pygame.K_RETURN:
+                self.input = self.text  # Update the input variable
+                return self.text
+            else:
+                self.text += event.unicode
+            # Update the text surface whenever text changes
+            self.txt_surface = self.font.render(self.text, True, self.color)
+    
+    def draw(self):
+        # Draw the rect
+        pygame.draw.rect(SCREEN, self.color, self.rect, 2)
+        # Draw the text
+        SCREEN.blit(self.txt_surface, (self.rect.x + 5, self.rect.y + 5))
