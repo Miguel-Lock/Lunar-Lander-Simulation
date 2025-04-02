ccc390f8eaed3cde6c2f69671e0b9938dbe27eba
Joshua <joshfouch777@gmail.com>
2025-04-01 21:17:15 -0400

    started

--- Screens/customize.py
+++ Screens/customize.py
@@ -1,25 +1,48 @@
 import pygame
 from game_state_manager import BaseState
 from constants import SCREENWIDTH, SCREENHEIGHT, FONT, SCREEN
-from gui_code.buttons import Button, backtomenu_button_img, start_button_img
+from gui_code.buttons import Button, backtomenu_button_img, start_button_img, button_img
 from gui_code.buttons import InputBox
 
-text = ""
-input_active = True
+# global variables for Miguel
+isRover = False
+astronaut_amt = 0   # should be 1-3
+instrument_num = 0  # should be 1-300
 
+isFinished = False
 
 class Customize(BaseState):
     def __init__(self, screen, gameStateManger):
         super().__init__(screen, gameStateManger)
         self.display = screen
+
         # Load background
         self.background = pygame.image.load(
             "Screens/backgrounds/customizebackground.png").convert_alpha()
-        self.bruh = InputBox(850, 450, 140, 60, test)
         
-    def run(self):
-        # Draw background
-        self.display.blit(self.background, (0, 0))
+       
+    def run(self):    
+
+        rover_text = FONT.render('Is there a Rover?', True, (255, 255, 255))
+        astronaut_text = FONT.render('How many Astronauts? (1-3)', True, (255, 255, 255))
+        instrument_text = FONT.render('How many Instruments? (0-300)', True, (255, 255, 255))
+
+        # is there a rover?
+        self.display.blit(rover_text, (950, 300))
+        yes_button = Button(950, 450, button_img, 1)
+        no_button = Button(950, 550, button_img, 1)
+
+        if yes_button.draw():
+            isRover = True
+            print("Yes")
+        elif no_button.draw():
+            isRover = False
+            print("No")
+
+       
+        
+        # astronaut count 1-3
+        # mass of additional instruments (0 - 300)
 
         # Back to menu button
         menu_button = Button(50, 50, backtomenu_button_img, 1)
--- gui_code/buttons.py
+++ gui_code/buttons.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import SCREEN, FONT
+clock = pygame.time.Clock()
 
 # instantiate the images as variables
 start_button_img = pygame.image.load(
@@ -10,6 +11,8 @@ exit_button_img = pygame.image.load(
     'gui_code/buttons/exit_button.png').convert_alpha()
 backtomenu_button_img = pygame.image.load(
     'gui_code/buttons/BacktoMenu.png').convert_alpha()
+button_img = pygame.image.load(
+    'gui_code/buttons/default_button.png').convert_alpha()
 
 # button class to declare button instances
 class Button():
@@ -52,39 +55,42 @@ class Button():
 
 
 class InputBox:
-    def __init__(self, x, y, width, height, input=''):
+    def __init__(self, x, y, width, height, input_text=''):
         self.rect = pygame.Rect(x, y, width, height)
         self.color_inactive = pygame.Color('lightskyblue3')
         self.color_active = pygame.Color('dodgerblue2')
         self.color = self.color_inactive
         self.font = FONT
-        self.text = input
-        self.txt_surface = self.font.render(self.text, True, self.color)
+        self.text = input_text
+        self.txt_surface = self.font.render(self.text, True, (255, 0, 0))
         self.active = False
-        self.input = input
 
     def handle_event(self, event):
         if event.type == pygame.MOUSEBUTTONDOWN:
+            # Toggle active state on click inside the box
             if self.rect.collidepoint(event.pos):
                 self.active = True
                 self.color = self.color_active
+                # Optionally clear the text on click:
+                # self.text = ""
             else:
                 self.active = False
                 self.color = self.color_inactive
-        
-        if event.type == pygame.KEYDOWN and self.active:  # Only handle key events if active
+
+        if event.type == pygame.KEYDOWN and self.active:
             if event.key == pygame.K_BACKSPACE:
                 self.text = self.text[:-1]
             elif event.key == pygame.K_RETURN:
-                self.input = self.text  # Update the input variable
-                return self.text
+                # On Enter, disable editing (or perform an action)
+                self.active = False
+                self.color = self.color_inactive
             else:
                 self.text += event.unicode
-            # Update the text surface whenever text changes
-            self.txt_surface = self.font.render(self.text, True, self.color)
-    
+            # Update the text surface after any change.
+            self.txt_surface = self.font.render(self.text, True, (255, 0, 0))
+
     def draw(self):
-        # Draw the rect
+        # Draw the input box border
         pygame.draw.rect(SCREEN, self.color, self.rect, 2)
-        # Draw the text
+        # Blit the current text.
         SCREEN.blit(self.txt_surface, (self.rect.x + 5, self.rect.y + 5))
