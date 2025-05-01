45db499322f863a121d12259aaeb994ed25dfc2b
Joshua Fouch <162926985+joshuafouch@users.noreply.github.com>
2025-05-01 14:20:49 -0400

    Warning Message when Rocket is too fast (#222)
    * added warning message
    
    * changed HUD to red when going to fast, warning message

--- Screens/simulation.py
+++ Screens/simulation.py
@@ -3,7 +3,7 @@ import time  # for delay between landing and results
 import sqlite3 # for database entries
 from game_state_manager import BaseState
 # from pygame.locals import RLEACCEL
-from constants import SCREENWIDTH, SCREENHEIGHT, SURFACE, FONT, SCREEN, ROCKET_BOTTOM, PXPERMETER, METERPERPX
+from constants import SCREENWIDTH, SCREENHEIGHT, SURFACE, FONT, BIGFONT, SCREEN, ROCKET_BOTTOM, PXPERMETER, METERPERPX
 from algos import MyAlgos
 from gui_code.buttons import Button, exit_button_img
 
@@ -26,6 +26,9 @@ tilapia_success_img = pygame.image.load(
 # importing rocket crash png
 tilapia_crash_img = pygame.image.load(
     'Screens/rocket_assets/TilapiaSad.png').convert_alpha()
+
+warning_msg = BIGFONT.render('!!WARNING-SLOWDOWN!!', True, (255, 0, 0))
+
 
 # Simulation screen
 
@@ -66,6 +69,10 @@ class Simulation(BaseState):
         if not self.rocket.is_landed:
             self.elapsed_time = (pygame.time.get_ticks() - self.start_time) / 1000
 
+            # WARNING MESSAGE IF GOING TO FAST!
+            if abs(self.rocket.algos.velocity) > 200:
+                self.display.blit(warning_msg, (((SCREENWIDTH//2) - 240), ((SCREENHEIGHT//2) - 500)))
+
         info_lines = [
             f"Velocity: {abs(int(self.rocket.algos.velocity))} m/s",
             f"Fuel Remaining: {int(self.rocket.algos.mass)} kg",
@@ -75,9 +82,15 @@ class Simulation(BaseState):
         ]
         # Text for rocket info
         line_height = FONT.get_height()
+       
         for i, line in enumerate(info_lines):
-            line_surface = FONT.render(line, True, (255, 255, 255))
+            # changes to red if rocket is going too fast!!!!
+            if abs(self.rocket.algos.velocity) > 200:
+                line_surface = FONT.render(line, True, (255, 0, 0))
+            else:
+                line_surface = FONT.render(line, True, (255, 255, 255))
             self.display.blit(line_surface, (50, 50 + i * line_height))
+
 
         # Buttons for screen directions
         exit_button = Button(1660, 50, exit_button_img, 1)
@@ -161,6 +174,7 @@ class OurFavoriteRocketShip(pygame.sprite.Sprite):
             else:
                 self.surf = tilapia_idle_img
                 self.is_thrust = False
+
 
         # Check if rocket has landed
         if self.rect.bottom >= SURFACE:
